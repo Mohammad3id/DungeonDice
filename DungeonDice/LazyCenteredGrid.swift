51b09fa93cf684787011f9378db787c63749fb21
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-10-31 10:11:40 +0300

    implement dungeon dice app

---
+++ DungeonDice/LazyCenteredGrid.swift
@@ -0,0 +1,18 @@
+//
+//  LazyCenteredGrid.swift
+//  DungeonDice
+//
+//  Created by Mohammad Eid on 31/10/2024.
+//
+
+import SwiftUI
+
+struct LazyCenteredGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyCenteredGrid()
+}
---
+++ DungeonDice/TitleView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleView.swift
+//  DungeonDice
+//
+//  Created by Mohammad Eid on 31/10/2024.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleView()
+}
