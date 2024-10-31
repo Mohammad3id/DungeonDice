ceee9d648a7c05e4d6197b05bed16cdd28f5fd10
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-10-31 06:00:47 +0300

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Mohammad Eid on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
